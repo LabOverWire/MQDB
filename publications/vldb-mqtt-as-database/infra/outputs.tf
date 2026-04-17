1e1d922deecbb231500e1d8cd55d03f86fc8c45b
Fabrício Bracht <fabracht@gmail.com>
2026-04-16 20:21:32 -0700

    fix terraform outputs to match aws_run.sh expectations

--- publications/vldb-mqtt-as-database/infra/outputs.tf
+++ publications/vldb-mqtt-as-database/infra/outputs.tf
@@ -1,14 +1,24 @@
-output "instance_ip" {
-  description = "Public IP of the benchmark instance"
-  value       = aws_instance.bench.public_ip
-}
-
 output "instance_id" {
   description = "EC2 instance ID"
   value       = aws_instance.bench.id
 }
 
+output "public_dns" {
+  description = "Public DNS of the benchmark instance"
+  value       = aws_instance.bench.public_dns
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "ami_id" {
+  description = "AMI ID used for the instance"
+  value       = data.aws_ami.ubuntu.id
+}
+
 output "ssh_command" {
   description = "SSH command to connect to the instance"
-  value       = "ssh -i ~/.ssh/id_ed25519 ubuntu@${aws_instance.bench.public_ip}"
+  value       = "ssh -i ~/.ssh/id_ed25519 ubuntu@${aws_instance.bench.public_dns}"
 }
